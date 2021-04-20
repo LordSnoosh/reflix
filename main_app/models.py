dc67e5088ae88df734413dd7c099c22b5308cab4
dshindel <dannyshindel@gmail.com>
2021-04-20 09:49:53 -0700

    new model

--- main_app/models.py
+++ main_app/models.py
@@ -5,6 +5,7 @@ from django.db import models
 class User(models.Model):
     name = models.CharField(max_length=100)
     email = models.EmailField(max_length=100)
+    date_created = models.DateTimeField(auto_now=True)
 
     def __str__(self):
         return f'{self.name}/{self.email}'
